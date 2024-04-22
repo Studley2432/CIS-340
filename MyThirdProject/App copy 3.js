dbccd777a9baf95b9f42cfaa453f239c907f2cbb
Studley2432 <117372506+Studley2432@users.noreply.github.com>
2024-04-21 20:18:20 -0600

    updated Project 3
    List View & Section List

---
+++ MyThirdProject/App copy 3.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Text, Image, ScrollView, FlatList } from 'react-native';
+
+const dog = {
+  uri: 'https://www.princeton.edu/sites/default/files/styles/1x_full_2x_half_crop/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=Bg2K7j7J',
+  width: 128,
+  height: 128
+
+}
+
+export default StatesApp = () => {
+  return (
+    <View style={{flex: 1, paddingTop:22}}>
+      <FlatList
+      data={[
+        {key: 'Alabama'},
+        {key: 'Alaska'},
+        {key: 'Arkansas'},
+        {key: 'California'},
+        {key: 'Colorado'},
+        {key: 'Delaware'},
+        {key: 'Florida'},
+        {key: 'Georgia'},
+        {key: 'Hawaii'},
+        {key: 'Idaho'},
+        {key: 'Illinois'},
+        {key: 'Arkansas'},
+        {key: 'Indiana'},
+        {key: 'Colorado'},
+        {key: 'Iowa'},
+        {key: 'Kentucky'},
+        {key: 'Maine'},
+        {key: 'Maryland'},
+        {key: 'Massachusetts'},
+        {key: 'Michigan'},
+        {key: 'Minnesota'},
+        {key: 'Mississippi'},
+        {key: 'Utah'},
+      ]}
+
+      returnItem={({item}) => <Text style={{padding: 10, fontSize: 20, height: 44}}> {item.key}</Text> }
+
+      />
+
+
+
+
+
+
+
+    </View>
+);
+}
+
+
+
